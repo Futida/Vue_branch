c9a9d543cbccbe48cbc66ca2b507abffbfd9ed3d
konstantingrushin <konstantin.grushin@goods.ru>
2018-03-16 16:19:36 +0300

    commit

--- brunch-config.js
+++ brunch-config.js
@@ -1,10 +1,16 @@
 module.exports = {
     files: {
         javascripts: {
-            joinTo: 'app.js'
+            entryPoints: {
+                'src/main.js': 'bundle.js',
+            }
         }
     },
     paths: {
-        watched: ['src'] // use watched this, because default watched directory 'app'
+        watched: ['src'], // use watched this, because default watched directory 'app'
+        public: 'www/' // rename output directory
+    },
+    stylesheets: {
+        joinTo: 'app.css'
     },
 }
